923217c76f353e89b73b4ca695d4432ad6336d28
Pandamacika <alomvar_1@yahoo.com>
2022-11-21 13:14:02 +0200

    Rendezesek_1_Feladat

--- 02 modul/Rendezesek_1_Feladat/src/Main.java
+++ 02 modul/Rendezesek_1_Feladat/src/Main.java
@@ -1,12 +1,10 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.FileReader;
+import java.io.*;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -30,10 +28,11 @@ public class Main {
         Path newDirectory = Files.createDirectories(Path.of(uj_mappa_eleresi_utvonala));
         System.out.println("uj mappa = " + newDirectory);
 
-        Path newFile = Files.createDirectories(newDirectory.resolve(newDirectory+"/"+fajlok+"/"));
+        Path newFile = Files.createDirectories(newDirectory.resolve(newDirectory + "/" + fajlok + "/"));
         System.out.println("newFile = " + newFile);
-        Path ujFajlUtja = Paths.get("_DataFiles",fajlok,fajlok+".txt");
+        Path ujFajlUtja = Paths.get("_DataFiles", fajlok, fajlok + ".txt");
         File ujFajl = ujFajlUtja.toFile();
+
         FileWriter fw = new FileWriter(ujFajl);
         fw.write("6");
         fw.write("\n");
@@ -48,20 +47,43 @@ public class Main {
         fw.write("Narancs");
         fw.write("\n");
         fw.write("Szolo");
-
-
+        fw.write("\n");
+        fw.write("\n");
         fw.close();
-
-
         FileReader fr = new FileReader(ujFajl);
         Scanner scFileOlvaso = new Scanner(fr);
         String elsoSor = scFileOlvaso.nextLine();
         System.out.println(elsoSor);
-        String [] tomb = new String[Integer.parseInt(elsoSor)];
-        for (int i=0;i<Integer.parseInt(elsoSor);i++){
-            tomb[i]=scFileOlvaso.nextLine();
+        String[] tomb = new String[Integer.parseInt(elsoSor)];
+        String[] elso_betu = new String[Integer.parseInt(elsoSor)];
+
+        for (int i = 0; i < Integer.parseInt(elsoSor); i++) {
+            tomb[i] = scFileOlvaso.nextLine();
             System.out.println(tomb[i]);
         }
+        for (int i = 0; i < Integer.parseInt(elsoSor); i++) {
+            elso_betu[i] = String.valueOf(tomb[i].charAt(0));
+            Arrays.sort(tomb);
+            System.out.print(elso_betu[i] + ",");
+
+        }
+        System.out.println();
+        for (int i = 0; i < Integer.parseInt(elsoSor); i++) {
+            Arrays.sort(elso_betu);
+            System.out.print(elso_betu[i] + "," + tomb[i] + ",");
+
+        }
         fr.close();
+
+        BufferedWriter output = new BufferedWriter(new FileWriter(ujFajl, true));
+        for (int i = 0; i < Integer.parseInt(elsoSor); i++) {
+            String osszefuzve = elso_betu[i] + "," + tomb[i] + ",";
+            output.append(osszefuzve);
+        }
+        output.close();
+        // Buborekos rendezes
+
     }
+
+
 }
