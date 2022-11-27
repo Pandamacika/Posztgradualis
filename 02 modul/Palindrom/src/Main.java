b254987493cc1926026d6adb1e8149f02baef89b
Pandamacika <alomvar_1@yahoo.com>
2022-11-27 20:35:58 +0200

    Modul algoritmusok

--- 02 modul/Palindrom/src/Main.java
+++ 02 modul/Palindrom/src/Main.java
@@ -19,23 +19,33 @@ public class Main {
         System.out.println(uj_szoveg);
         int hossz = uj_szoveg.length();
         Szoveget_terit_vissza(uj_szoveg);
-
+        int palindrom_vagy_sem=0;
         for (int i = 0; i < hossz; i++) {
-
-            System.out.print(uj_szoveg.charAt(hossz - i - 1));
+            if (uj_szoveg.charAt(0) != uj_szoveg.charAt(hossz - 1)) {
+                System.out.println( uj_szoveg.charAt(hossz - i - 1));
+                palindrom_vagy_sem++;
+            } else {
+                System.out.print(uj_szoveg.charAt(hossz - i - 1));
+                palindrom_vagy_sem=0;
+            }
+        }
+        System.out.println();
+        if (palindrom_vagy_sem==0){
+            System.out.println(" Ez palindrom ! ");
+        }
+        else {
+            System.out.println(" Ez nem palindrom ! ");
         }
 
     }
 
     private static char Szoveget_terit_vissza(String uj_szoveg) {
         int hossz = uj_szoveg.length();
-        char eredmeny = uj_szoveg.charAt(hossz-1);
-        if (uj_szoveg.charAt(0) != uj_szoveg.charAt(hossz  - 1))
-        {
+        char eredmeny = uj_szoveg.charAt(hossz - 1);
+        if (uj_szoveg.charAt(0) != uj_szoveg.charAt(hossz - 1)) {
             return 0;
-        }
-        else {
-            return Szoveget_terit_vissza(uj_szoveg.substring(1,hossz-1));
+        } else {
+            return Szoveget_terit_vissza(uj_szoveg.substring(1, hossz));
         }
 
     }
