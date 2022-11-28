5e728e261a3b0ddd04bb62f2b5ee36a0cadbf8c1
Pandamacika <alomvar_1@yahoo.com>
2022-11-28 15:22:54 +0200

    Modul algoritmusok

--- 02 modul/Fajlok_4_Feladat/src/Main.java
+++ 02 modul/Fajlok_4_Feladat/src/Main.java
@@ -12,23 +12,30 @@ public class Main {
         //Írj egy programot ami addig kér be könyvtárneveket,
         // ameddig abban pont nem lesz (pl x, y, a.txt),
         //majd ez alapján létrehozza az aktuális könyvtárban az x/y/a.txt fájlt.
-        System.out.println(" Ez a program konyvtarakat es egy fajlt hoz letre ! ");
+        System.out.println(" Ez a program txt fajlokat hoz letre,amig pontot nem kap eredmenykent ! ");
         Path mappa_eleresi_utvonala = Paths.get("", "_DataFiles").toAbsolutePath();
         String uj_mappa_eleresi_utvonala = mappa_eleresi_utvonala + "/";
         File mappa = mappa_eleresi_utvonala.toFile();
 
         Scanner sc = new Scanner(System.in);
-        String kilepes = ".txt";
+        String kilepes = ".";
         String fajlok = "";
         while (!Objects.equals(fajlok, kilepes)) {
             fajlok = sc.nextLine();
 
             if (!Objects.equals(fajlok, kilepes)) {
                 Path newDirectory = Files.createDirectories(Path.of(uj_mappa_eleresi_utvonala));
-                System.out.println("uj mappa = " + newDirectory);
-                Path uj_mappa = Paths.get(uj_mappa_eleresi_utvonala,fajlok,"/");
-                Path newFile = Files.createDirectories(newDirectory.resolve(newDirectory+"/"+fajlok+"/"));
-                System.out.println("newFile = " + newFile);
+                //System.out.println("uj mappa = " + newDirectory);
+                boolean mielott_fajlt_keszitene = Files.exists(Path.of(newDirectory + "/" + fajlok + ".txt"));
+
+
+                if (!mielott_fajlt_keszitene) {
+                    Path newFile = Files.createFile(newDirectory.resolve(newDirectory + "/" + fajlok + ".txt"));
+                    // System.out.println("newFile = " + newFile);
+                } else {
+                    System.out.println(" Ez a fajl mar letezik, kerem irjon be uj nevet :");
+                    fajlok = sc.nextLine();
+                }
             } else {
                 System.out.println("Vege");
             }
