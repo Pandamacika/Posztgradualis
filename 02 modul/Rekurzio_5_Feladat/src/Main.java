3a485b4784e1c2be0db5f441a36818af708f1d38
Pandamacika <alomvar_1@yahoo.com>
2023-01-26 11:52:19 +0200

    Modul algoritmusok

--- 02 modul/Rekurzio_5_Feladat/src/Main.java
+++ 02 modul/Rekurzio_5_Feladat/src/Main.java
@@ -13,21 +13,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.print(" A tomb elemei : ");
-        for (int i=0;i<n;i++){
-        System.out.print(Fibonacci(i)+",");
+        for (int i = 0; i < n; i++) {
+            System.out.print(Fibonacci(i) + ",");
         }
 
-
     }
-
     private static int Fibonacci(int n) {
-        int [] tomb = new int[n];
-        if (n==0){return 1;}
-        else if (n==1){return 1;}
-        else {return Fibonacci(n-1)+Fibonacci(n-2);}
 
-
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
     }
-
-
 }
